4621cf60b1136c39744ccfedffa5fa862fc70770
JYIsaac <joanneissac2021@u.northwestern.edu>
2021-07-13 15:52:15 -0500

    removed test file

--- test.py
+++
@@ -1,5 +0,0 @@
-print('hello world')
-
-print('my name is joanne, I don't know how to spell my name')
-
-print('my name is jay, I ate 5 hot dogs for a talent show in front my company...I did not get the raise')
